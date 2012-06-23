d1539ddce04739d42481113f0e0a2841e39b265b
George Hickman <george@ghickman.co.uk>
2012-06-23 23:41:11 +0100

    unique on both name and env

--- core/models.py
+++ core/models.py
@@ -3,11 +3,14 @@ from django_hstore import hstore
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     environment = models.CharField(max_length=255)
     secrets = hstore.DictionaryField(db_index=True)
 
     objects = hstore.HStoreManager()
+
+    class Meta:
+        unique_together = ('name', 'environment')
 
     def __unicode__(self):
         return self.name
