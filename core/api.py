b9b35481a941ac811cfdb7602f3f1c4cda425fa1
George Hickman <george@ghickman.co.uk>
2012-06-23 23:40:30 +0100

    Use filtering instead of bespoke url
    Can't seem to filter on top of a bespoke url.

--- core/api.py
+++ core/api.py
@@ -14,20 +14,14 @@ class ProjectResource(ModelResource):
     class Meta:
         allowed_methods = ('get',)
         fields = ('name', 'environment', 'secrets')
-        filtering = {'environment': ('exact',)}
+        filtering = {
+            'environment': ('exact',),
+            'name': ('exact',)
+        }
         # authentication = BasicAuthentication()
         # authorization = DjangoAuthorization()
         queryset = Project.objects.all()
 
     def dehydrate_secrets(self, bundle):
         return [{k: v} for k, v in bundle.data['secrets'].items()]
-
-    def override_urls(self):
-        """
-        http://django-tastypie.rtfd.org/en/latest/cookbook.html#using-non-pk-data-for-your-urls
-        """
-        return [
-            url(r'^(?P<resource_name>%s)/(?P<name>[\w\d_-]+)/$' % self._meta.resource_name,
-                self.wrap_view('dispatch_detail'), name='api_dispatch_detail'),
-        ]
 
