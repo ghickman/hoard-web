26cd861c35e742b1ac38f4a459c5c02d1a73d100
George Hickman <george@ghickman.co.uk>
2012-08-23 11:48:24 +0100

    Make the admin a bit more useful

--- trove/admin.py
+++ trove/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Env, Project
 
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'env')
+    list_filter = ('name', 'env')
+    search_fields = ('name',)
+
+
+admin.site.register(Env)
+admin.site.register(Project, ProjectAdmin)
